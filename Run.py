e22ff623a53a9bb1c28156e151bff355d7e74e0d
AFGHAN HACK <110977192+AfghanHack001@users.noreply.github.com>
2022-10-30 08:05:39 +0430

    Update Run.py

--- Run.py
+++ Run.py
@@ -4,5 +4,5 @@ arch=uname().machine.lower()
 if 'aarch' in arch:
     arch = 'aarch'
     print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools')
-    import Active
+    import Active.sh
 else:exit('\033[1;31m Sorry System or device not supported ')
